4e8290e88a74c5cc85aac633cb4c483331443ded
Jolly♥ <joellewagner19@yahoo.de>
2023-12-06 12:34:34 +0100

    Sky/Ground

--- Assets/Scripts/BeeMovements.cs
+++ Assets/Scripts/BeeMovements.cs
@@ -7,7 +7,8 @@ public class MovementScript : MonoBehaviour
     public void MoveBee(string json)
     {
         Vector3 data = JsonUtility.FromJson<Vector3>(json);
-        transform.position = new Vector3(data.x * 0.1f, data.y * 0.1f, data.z * 0.01f);
+        //transform.position = new Vector3(data.x * 0.1f, data.y * 0.1f, data.z * 0.01f);
+        transform.position = new Vector3(transform.position.x, (data.y * 0.1f)-1 , transform.position.z);
     }
 
     public void MoveBeeY(float y)
